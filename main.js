b6fedfbc322d4976ebcd18a2713210a8685097e2
Roland Bisasso <51475330+kamrold@users.noreply.github.com>
2019-06-17 22:42:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var times = prompt ("Please enter number of times");// prompts for the number of times function should input a number
+var times = prompt ("Please enter number of times");//(global scope) prompts for the number of times function should input a number
 function myGame(times) {
 	
 	var i = Math.floor(Math.random() * 100) + 1; //Generates random numbers and stores it in i
 
-	var j=i%2; //gets the modulus of the random number and stores the result in a variable j
+	var j=i%2; //gets the modulus of the random number and stores the result in a variable j (function scope)
 	if (j==0) //checks if j returns a zero or not
 	    alert (" Number is Even"); // if result is zero then j = even
 	else
@@ -11,7 +11,7 @@ function myGame(times) {
 
 	for(var count=1;count<=times; count++) // loop to counter the number of captured inputs
 	{
-	    var num = prompt ("Please enter a number:"); //Captures user input
+	    var num = prompt ("Please enter a number:"); //Captures user input .....e.g of function scope
 	    if (num==i){																// condition checks if auto gen. number is equal to captured number it terminates
 	        alert("You WIN!", +"Clapp for yourself" );
 	        break;
